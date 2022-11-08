4696f4e41c5d06fb9cb55937dc7b1c3b047a3b19
watermelon36 <106351185+watermelon36@users.noreply.github.com>
2022-11-08 17:39:51 -0500

    Add files via upload

---
+++ main_85.js
@@ -0,0 +1,27 @@
+menu_list_array = [ 
+    "Veg Margherita Pizza",                  
+    "Chicken Pineapple Pizza",
+    "Hawain Pizza",
+    "Paneer Pizza",  
+    "Cheese Pizza",
+    "Deluxe Meat Pizza",
+    "Deluxe Vegetable Pizza"]; //Add more Pizza item names
+
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    //Give the appropriate id name as display_menu 
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_suggestion(){
+    //Give the appropriate id name as add_item
+	var item = document.getElementById("add_item").value;
+    //Use the push() function to push the item into menu_list_array
+    menu_list_array.push(item);
+}
+
